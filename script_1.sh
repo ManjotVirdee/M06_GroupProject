50f2157710d1a4eabc2aa83b6ba3fc70e660e320
gmbaranzoni <gmbaranzoni@gmail.com>
2017-12-03 23:06:42 +0000

    Script 1 draft complete

--- script_1.sh
+++ script_1.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-##### FILTERING AND QUALITY CONTROL
+##### PART 1 
+##### Quality control and aligment of Daphina magna genomes in the "DmagnaSamples_subset" directory
+
+
 ### MODULE TO LOAD
 #   module load apps/fastqc/v0.11.2  
-#   module load apps/trimgalore/v0.4.2
 #   module load apps/trimmomatic
+#   module load apps/samtools
+#   module load apps/BWA
 
 
 ### VARIABLES FOLDERS
 project_folder='/rds/projects/2017/orsinil-bioinfoproj/VariantCall_analysis/'
 raw_data_folder=$project_folder'DmagnaSamples_subset/'
 
-### MAKE DIRECTORIES - the option p is to avoid the creation of a directory if it already exist
-fastqc_folder=$project_folder"FastQC"
-	mkdir -p $fastqc_folder
-fastqc_rawdata=$project_folder"FastQC/raw_data"
-	mkdir -p $fastqc_rawdata
-fastqc_trimgalore=$project_folder"FastQC/TrimGalore_data"
-	mkdir -p $fastqc_trimgalore
-fastqc_trimmomatic=$project_folder"FastQC/Trimmomatic_data"
-	mkdir -p $fastqc_trimmomatic
-trimgalore_data=$project_folder"TrimGalore_data/"
-	mkdir -p $trimgalore_data
-trimmomatic_data=$project_folder"Trimmomatic_data/"
-	mkdir -p $trimmomatic_data
+
+### Indexing the reference genome (bwtsw for large genomes)
+
+	# Initilise variables 
+export Dmagna_reference=$project_folder'reference_genome/GCA_001632505.1_daphmag2.4_genomic.fna'
+
+	# Software commands
+bwa index -a bwtsw $Dmagna_reference  # bwtsw for large genomes
 
 
-#### RUN EACH FILE
+
+#### ANALYSE EACH FILE
 # In order to follow all the variable substitutions, one example for each variable is added as a comment.
 # All the "echo" commands are used to debug the script.
 
 for fastq_forward in $raw_data_folder*"-R1.fastq.gz";   # All the files that end with "-R1.fastq.gz"  
 do
-	### List of the Daphnias
+
+### List of the Daphnias
 	export fastq_forward 
 		# /rds/projects/2017/orsinil-bioinfoproj/VariantCall_analysis/DmagnaSamples_subset/Dmagna1-0_1-L002-R1.fastq.gz
 	export fastq_reverse="$( echo $fastq_forward | sed -e 's/-R1.fastq.gz/-R2.fastq.gz/')" 
@@ -41,8 +42,8 @@ do
 		# Dmagna1-0_1-L002-R1.fastq.gz
 	export fastq_file_reverse="$( echo $fastq_file_forward | sed -e 's/-R1.fastq.gz/-R2.fastq.gz/')" 
 		# Dmagna1-0_1-L002-R2.fastq.gz
-	export fastq_base="${fastq_file_forward%%-*}" # I need to change this!
-		# Dmagna1 
+	export fastq_base= "$( echo $fastq_file_forward | sed -e 's/-L00[0-9]-R[1-2].fastq.gz//')"
+		# Dmagna1-0_1 
 		echo $fastq_forward
 		echo $fastq_reverse
 		echo $fastq_file_forward
@@ -50,34 +51,55 @@ do
 		echo $fastq_base
 
 
-	# Run FastQC on raw data
+### Run FastQC on raw data
+		
+		# Initilise variables and folders 
+	fastqc_folder=$project_folder"FastQC"
+	mkdir -p $fastqc_folder
+	fastqc_rawdata=$project_folder"FastQC/raw_data"
+	mkdir -p $fastqc_rawdata
+		
+		# Software commands
 	fastqc $fastq_forward -o $fastqc_rawdata
 	fastqc $fastq_reverse -o $fastqc_rawdata
 
-	# Run TrimGalore on raw data
-	trim_galore --trim-n --illumina --paired $fastq_forward $fastq_reverse -o $trimgalore_data
-	mv $trimgalore_data$fastq_file_forward"_trimming_report.txt" $fastqc_trimgalore
-	mv $trimgalore_data$fastq_file_reverse"_trimming_report.txt" $fastqc_trimgalore
 
-	# Run FastQC on TrimGalore data
-	fastqc $fastq_forward -o $fastqc_trimgalore
-	fastqc $fastq_reverse -o $fastqc_trimgalore
+### Run Trimmomatic on raw data
 
-	# Run Trimmomatic on raw data
-	export trimgalore_forward=$trimgalore_data"$( echo $fastq_file_forward | sed -e 's/.fastq.gz/_val_1.fq.gz/')"
-	export trimgalore_reverse=$trimgalore_data"$( echo $fastq_file_reverse | sed -e 's/.fastq.gz/_val_2.fq.gz/')"
+		# Initilise variables and folders 
+	trimmomatic_data=$project_folder"Trimmomatic_data/"
+	mkdir -p $trimmomatic_data
 	export trimmed_output=$trimmomatic_data$fastq_base
-	trimmomatic PE -phred33 $trimgalore_forward $trimgalore_reverse -baseout $trimmed_output LEADING:3 TRAILING:3 SLIDINGWINDOW:80:25 MINLEN:35 
+	export adapter_fasta=$project_folder"TruSeq3-PE-2.fa"
 
-	echo $trimgalore_forward
-	echo $trimgalore_reverse
-	echo $trimmed_output
+		# Software commands
+	trimmomatic PE $fastq_forward $fastq_reverse -baseout $trimmed_output CROP:225 ILLUMINACLIP:$adapter_fasta:2:30:10 LEADING:10 TRAILING:10 SLIDINGWINDOW:50:20 MINLEN:36
+	
 
-	# Run FastQC of Trimmomatic data
+### Run FastQC of Trimmomatic data
+
+		# Initilise variables and folders 
+	fastqc_trimmomatic=$project_folder"FastQC/Trimmomatic_data"
+	mkdir -p $fastqc_trimmomatic
+
+		# Software commands
 	fastqc $trimmed_output"_1P" -o $fastqc_trimmomatic
 	fastqc $trimmed_output"_2P" -o $fastqc_trimmomatic
+	fastqc $trimmed_output"_1U" -o $fastqc_trimmomatic
+	fastqc $trimmed_output"_2U" -o $fastqc_trimmomatic
 
-echo ""
+
+### Run BWA of Trimmomatic data
+
+		# Initilise variables and folders 
+	bwa_data=$project_folder"BWA/"
+	mkdir -p $bwa_data
+	export bwa_output=$bwa_data$fastq_base".sorted.bam"
+
+		# Software commands
+	bwa mem $Dmagna_reference $trimmed_output"_1P" $trimmed_output"_2P" | samtools view -bh | samtools sort > $bwa_output
+
+
 done
 
 
